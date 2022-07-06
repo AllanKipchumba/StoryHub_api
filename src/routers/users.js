160f77b7385203905376316406db3fe076938c8c
_Allan <allankipchumba76@gmail.com>
2022-07-06 10:44:01 +0300

    updated get user profile image route

--- src/routers/users.js
+++ src/routers/users.js
@@ -129,22 +129,16 @@ router
         req.user.avatar = undefined;
         await req.user.save();
         res.send();
-    });
-
-// serving up user profile image
-router.get("/:id/avatar", async(req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-
-        if (!user || !user.avatar) {
-            throw new Error();
+    })
+    // serve up profile image
+    .get(auth, async(req, res) => {
+        try {
+            const user = req.user;
+            res.set("Content-Type", "image/png");
+            res.send(user.avatar);
+        } catch (error) {
+            res.status(404).send(e);
         }
-
-        res.set("Content-Type", "image/png");
-        res.send(user.avatar);
-    } catch (e) {
-        res.status(404).send(e);
-    }
-});
+    });
 
 module.exports = router;
