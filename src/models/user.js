a007e53f183357585d27f4c112bded23a56f8d41
allan <allankipchumba76@gmail.com>
2023-02-03 15:45:41 +0100

    send back user email only on auth

--- src/models/user.js
+++ src/models/user.js
@@ -4,94 +4,98 @@ const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
-        // run validation
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error("Invalid email address");
-            }
-        },
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      // run validation
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid email address");
+        }
+      },
     },
     password: {
-        type: String,
-        required: true,
-        minlength: 6,
-        trim: true,
-        // password should not be set to the string "password"
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error("Password can not contain 'password'");
-            }
-        },
+      type: String,
+      required: true,
+      minlength: 6,
+      trim: true,
+      // password should not be set to the string "password"
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password can not contain 'password'");
+        }
+      },
     },
-    tokens: [{
+    tokens: [
+      {
         token: {
-            type: String,
-            required: true,
+          type: String,
+          required: true,
         },
-    }, ],
-}, { timestamps: true });
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 // GENERATE AUTH TOKENS FOR USER
-userSchema.methods.generateAuthTokens = async function() {
-    const user = this;
-    // create token
-    const token = jwt.sign({ _id: user._id.toString() }, config.jwt_secret);
+userSchema.methods.generateAuthTokens = async function () {
+  const user = this;
+  // create token
+  const token = jwt.sign({ _id: user._id.toString() }, config.jwt_secret);
 
-    // save token to db
-    user.tokens = user.tokens.concat({ token });
-    await user.save();
+  // save token to db
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
 
-    return token;
+  return token;
 };
 
 // HASH PLAIN TEXT PASSWORD BEFORE SAVING TO DB
-userSchema.pre("save", async function(next) {
-    const user = this;
-    // when creating/updating password
-    if (user.isModified("password")) {
-        user.password = await bcrypt.hash(user.password, 8);
-    }
-    // makes sure to run next() to save the user
-    next();
+userSchema.pre("save", async function (next) {
+  const user = this;
+  // when creating/updating password
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  // makes sure to run next() to save the user
+  next();
 });
 
 // CHECK IF USER EMAIL EXISTS AND VALIDATE PASSWORD >>
 //{ .statics - TARGETS User}
-userSchema.statics.findByCredentials = async(email, password) => {
-    // email: email  destructerd to {email}
-    const user = await User.findOne({ email });
+userSchema.statics.findByCredentials = async (email, password) => {
+  // email: email  destructerd to {email}
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        throw new Error("Unable to login");
-    }
+  if (!user) {
+    throw new Error("Unable to login");
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error("Unable to login");
-    }
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
 
-    return user;
+  return user;
 };
 
 // HIDE USER'S SENSITIVE DATA >>
 //{ .methods - TARGETS user }
-userSchema.methods.toJSON = function() {
-    const user = this;
-    const userObject = user.toObject();
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
 
-    delete userObject.password;
-    delete userObject.tokens;
-    delete userObject.avatar;
+  delete userObject.password;
+  delete userObject.tokens;
 
-    return userObject;
+  return userObject;
 };
 
 const User = mongoose.model("User", userSchema);
--- src/routers/users.js
+++ src/routers/users.js
@@ -4,47 +4,54 @@ const User = require("../models/user");
 const auth = require("../middleware/auth");
 
 // sign up
-router.route("/register").post(async(req, res) => {
-    try {
-        const user = new User(req.body);
-        await user.save();
+router.route("/register").post(async (req, res) => {
+  try {
+    const createUser = new User(req.body);
+    await createUser.save();
 
-        // generate Auth tokens for user at sign up
-        const token = await user.generateAuthTokens();
-        res.status(201).send({ user, token });
-    } catch (error) {
-        res.status(400).json(`Error: ${error}`);
-        console.log(error);
-    }
+    // generate Auth tokens for user at sign up
+    const token = await createUser.generateAuthTokens();
+
+    const user = {
+      email: createUser.email,
+    };
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).json(`Error: ${error}`);
+    console.log(error);
+  }
 });
 
 // log in
-router.route("/login").post(async(req, res) => {
-    try {
-        const user = await User.findByCredentials(
-            req.body.email,
-            req.body.password
-        );
-        // generate Auth tokens for user at log in
-        const token = await user.generateAuthTokens();
-        res.send({ user, token });
-    } catch (error) {
-        res.status(400).send(`Error: ${error}`);
-    }
+router.route("/login").post(async (req, res) => {
+  try {
+    const identifyUser = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    // generate Auth tokens for user at log in
+    const token = await identifyUser.generateAuthTokens();
+    const user = {
+      email: identifyUser.email,
+    };
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(`Error: ${error}`);
+  }
 });
 
 // log out
-router.route("/logout").post(auth, async(req, res) => {
-    try {
-        // filter out the token belonging to the device the user used at log in
-        req.user.tokens = req.user.tokens.filter((token) => {
-            return token.token !== req.token;
-        });
-        await req.user.save();
-        res.send();
-    } catch (error) {
-        res.status(500).json(`Error: ${error}`);
-    }
+router.route("/logout").post(auth, async (req, res) => {
+  try {
+    // filter out the token belonging to the device the user used at log in
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).json(`Error: ${error}`);
+  }
 });
 
 module.exports = router;
